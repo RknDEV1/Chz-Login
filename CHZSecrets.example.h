f860745f5def08675c79622a2403e695c4e35c4e
RknDEV1 <rakinin22@icloud.com>
2026-08-27 13:30:08 -0300

    Delete CHZSecrets.example.h

--- CHZSecrets.example.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-// Copie este arquivo para CHZSecrets.h somente no ambiente privado de build.
-// Nunca faça commit do arquivo real.
-#define CHZ_API_TOKEN @"COLOQUE_SEU_TOKEN_AQUI"
-#define CHZ_STORE_URL @"https://discord.gg/dukkKRvz"

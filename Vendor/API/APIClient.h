2f7e7f4117c5cd91305b77e0159153ab74babef8
RknDEV1 <rakinin22@icloud.com>
2026-08-30 18:33:51 -0300

    Add files via upload

--- Vendor/API/APIClient.h
+++ Vendor/API/APIClient.h
@@ -1,43 +1,34 @@
-#ifndef APICLIENT_H
-#define APICLIENT_H
+// APIClient.h — Logos API Authentication / Lite Secure
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-#include <stdbool.h>
+NS_ASSUME_NONNULL_BEGIN
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+@interface APIClient : NSObject
+- (void)paid:(void (^)(void))execute;
+- (void)start:(void (^)(void))onStart init:(void (^)(void))init;
+- (void)setToken:(NSString *)token;
+- (void)setUDID:(NSString *)uid;
+- (void)setLanguage:(NSString *)language;
+- (void)hideUI:(BOOL)isHide;
+- (void)strictMode:(BOOL)isStrictMode;
+- (void)silentMode:(BOOL)isSilentMode;
+- (id)getPackageDataWithKey:(NSString *)key;
+- (NSString *)getKey;
+- (NSString *)getExpiryDate;
+- (NSString *)getExpiredAt;
+- (NSString *)getUDID;
+- (NSString *)getDeviceModel;
+- (NSString *)getLoginIP;
+- (NSString *)getPackageName;
+- (void)onCheckPackage:(void (^)(NSDictionary *header))success
+            onFailure:(void (^)(NSDictionary *error))failure;
+- (void)onCheckDevice:(void (^)(NSDictionary *data))success
+            onFailure:(void (^)(NSDictionary *error))failure;
+- (void)onLogin:(NSString *)inputKey
+      onSuccess:(void (^)(NSDictionary *data))success
+      onFailure:(void (^)(NSDictionary *error))failure;
++ (instancetype)sharedAPIClient;
+@end
 
-typedef void (^apiclient_callback)(void);
-typedef void (^apiclient_dict_callback)(const char* json);
-
-void apiclient_set_token(const char* token);
-void apiclient_set_udid(const char* udid);
-void apiclient_set_language(const char* language);
-void apiclient_set_contact_button_title(const char* title);
-void apiclient_set_description(const char* description);
-void apiclient_hide_ui(bool isHide);
-void apiclient_strict_mode(bool isStrictMode);
-void apiclient_silent_mode(bool isSilentMode);
-void apiclient_show_udid_guide(bool showGuide);
-void apiclient_set_window_mode(int mode);
-
-void apiclient_paid(apiclient_callback callback);
-
-const char* apiclient_get_key(void);
-const char* apiclient_get_udid(void);
-const char* apiclient_get_expired_at(void);
-const char* apiclient_get_expired_at_local(void);
-const char* apiclient_get_device_model(void);
-const char* apiclient_get_login_ip(void);
-const char* apiclient_get_package_name(void);
-const char* apiclient_get_package_data(const char* key);
-
-void apiclient_on_check_package(apiclient_dict_callback success, apiclient_dict_callback failure);
-void apiclient_on_check_device(apiclient_dict_callback success, apiclient_dict_callback failure);
-void apiclient_on_login(const char* inputKey, apiclient_dict_callback success, apiclient_dict_callback failure);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+NS_ASSUME_NONNULL_END
